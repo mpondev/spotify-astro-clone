504ee4594aa53b6325723f59d172a41a0120355c
Mario Ponce <mario@mpondev.com>
2024-02-02 18:47:40 +0100

    Create endpoint and API to get playlist info

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -5,8 +5,24 @@ export function CardPlayButton({ id }) {
   const { currentMusic, isPlaying, setCurrentMusic, setIsPlaying } =
     usePlayerStore(state => state);
 
+  const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id;
+
   const handleClick = () => {
-    setIsPlaying(!isPlaying);
+    if (isPlayingPlaylist) {
+      setIsPlaying(false);
+      return;
+    }
+
+    fetch(`/api/get-info-playlist.json?id=${id}`)
+      .then(res => res.json())
+      .then(data => {
+        const { songs, playlist } = data;
+
+        setIsPlaying(true);
+        setCurrentMusic({ songs, playlist, song: songs[0] });
+
+        console.log(songs, playlist);
+      });
   };
 
   return (
@@ -14,7 +30,7 @@ export function CardPlayButton({ id }) {
       onClick={handleClick}
       className="card-play-button rounded-full bg-green-500 p-4"
     >
-      {isPlaying ? <Pause /> : <Play />}
+      {isPlayingPlaylist ? <Pause /> : <Play />}
     </button>
   );
 }
