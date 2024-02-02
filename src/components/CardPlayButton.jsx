50f192cc5585f90e4348ccd3fceb2fd439d6e7f5
mpondev <mario@mpondev.com>
2024-02-02 11:44:44 +0100

    Add CardPlayButton

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,8 @@
+import { Pause, Play } from './Player';
+export function CardPlayButton({ id }) {
+  return (
+    <div className="card-play-button rounded-full bg-green-500 p-4">
+      <Play />
+    </div>
+  );
+}
