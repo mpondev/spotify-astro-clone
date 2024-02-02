5e055e20dc210883a536fc06ec310a0752eedd73
Mario Ponce <mario@mpondev.com>
2024-02-02 19:03:20 +0100

    Add play/pause functionality and change current song

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -59,22 +59,25 @@ export const Volume = () => (
 );
 
 export function Player() {
-  const { isPlaying, setIsPlaying } = usePlayerStore(state => state);
-  const [currentSong, setCurrentSong] = useState(null);
+  const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore(
+    state => state
+  );
   const audioRef = useRef();
 
   useEffect(() => {
-    audioRef.current.src = '/music/1/01.mp3';
-  }, []);
+    isPlaying ? audioRef.current.play() : audioRef.current.pause();
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const { song, playlist, songs } = currentMusic;
+    if (song) {
+      const src = `/music/${playlist?.id}/0${song.id}.mp3`;
+      audioRef.current.src = src;
+      audioRef.current.play();
+    }
+  }, [currentMusic]);
 
   const handleClick = () => {
-    if (isPlaying) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
-      audioRef.current.volume = 0.2;
-    }
-
     setIsPlaying(!isPlaying);
   };
 
